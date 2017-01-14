030b9afa5896682bc324a8080947ab6d505ae1d6
Eric Scuccimarra <skooch@gmail.com>
2017-01-14 11:41:08 +0100

    Fixing some minor things

--- src/sitemapServiceProvider.php
+++ src/sitemapServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace namespace Escuccim\Sitemap;
+namespace Escuccim\Sitemap;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -13,9 +13,42 @@ class sitemapServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
-    }
+        // use this if your package has views
+        $this->loadViewsFrom(realpath(__DIR__.'/resources/views'), 'escuccim');
 
+        // use this if your package has lang files
+        $this->loadTranslationsFrom(__DIR__.'/resources/lang', 'escuccim');
+
+        // use this if your package has routes
+        $this->setupRoutes($this->app->router);
+
+        // load our migrations
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        // publish config if necessary
+         $this->publishes([
+                 __DIR__.'/config/config.php' => config_path('blog.php'),
+                 __DIR__ . '/resources/views' => base_path('resources/views/vendor/escuccim')
+         ]);
+
+        // use the default configuration file as fallback
+         $this->mergeConfigFrom(
+             __DIR__.'/config/config.php', 'sitemap'
+         );
+    }
+    /**
+     * Define the routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    public function setupRoutes(Router $router)
+    {
+        $router->group(['namespace' => 'Escuccim\Sitemap\Http\Controllers'], function($router)
+        {
+            require __DIR__.'/Http/routes.php';
+        });
+    }
     /**
      * Register any package services.
      *
@@ -23,6 +56,17 @@ class sitemapServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->registerClass();
+
+        // specify the config file
+         config([
+                 'config/sitemap.php',
+         ]);
+    }
+    private function registerClass()
+    {
+        $this->app->bind('escuccim',function($app){
+            return new SiteMapClass($app);
+        });
     }
 }
