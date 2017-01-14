fd011a86d3cd8a082dad82a5f24b4bd296245908
Eric Scuccimarra <skooch@gmail.com>
2017-01-14 18:13:05 +0100

    Updated pages sitemap to use subdomain from config

--- src/resources/views/sitemap/pages.blade.php
+++ src/resources/views/sitemap/pages.blade.php
@@ -12,16 +12,18 @@
 				<lastmod>{{ date("c", strtotime($page->updated_at)) }}</lastmod>
 				<changefreq>{{ $page->changefreq }}</changefreq>
 				<priority>{{ $page->priority / 10 }}</priority>
-				@foreach(\Escuccim\Sitemap\Models\Subdomain::all() as $subdomain)
-					<xhtml:link rel="alternate"
-						hreflang="{{ $subdomain->language }}"
-						href="http://{{ $subdomain->subdomain ? $subdomain->subdomain . '.' : '' }}{{ app_domain() }}{{ $page->uri }}" />
-				@endforeach
+				@if(config('sitemap.hreflang_per_subdomain'))
+					@foreach(\Escuccim\Sitemap\Models\Subdomain::all() as $subdomain)
+						<xhtml:link rel="alternate"
+							hreflang="{{ $subdomain->language }}"
+							href="http://{{ $subdomain->subdomain ? $subdomain->subdomain . '.' : '' }}{{ app_domain() }}{{ $page->uri }}" />
+					@endforeach
 
 				@if(count(\Escuccim\Sitemap\Models\Subdomain::getDefault()))
 					<xhtml:link rel="alternate"
 						hreflang="x-default"
 						href="http://{{ \Escuccim\Sitemap\Models\Subdomain::getDefault()->subdomain ? \Escuccim\Sitemap\Models\Subdomain::getDefault()->subdomain . '.' : '' }}{{ app_domain() }}{{ $page->uri }}" />
+					@endif
 				@endif
 
 				@foreach($page->images as $image)
@@ -34,5 +36,4 @@
 			</url>
 		@endforeach
 	 @endforeach
-	 
 </urlset>
