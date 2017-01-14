4d4ffee828d7ecc8b5f7bd7bdb3d69512c0d4676
Eric Scuccimarra <skooch@gmail.com>
2017-01-14 14:38:00 +0100

    Added helper function and added them to autoload

---
+++ src/functions.php
@@ -0,0 +1,28 @@
+<?php
+
+if ( ! function_exists('app_domain')) {
+
+    /**
+     * Arrange for a flash message.
+     *
+     * @param  string|null $message
+     * @param  string      $level
+     * @return \Laracasts\Flash\FlashNotifier
+     */
+    function app_domain(){
+        $domain = env('APP_URL');
+        $domain = str_replace('http://', '', $domain);
+
+        if(isset($_SERVER['SERVER_NAME'])){
+            $pieces = explode('.', $_SERVER['SERVER_NAME']);
+            $subdomain = $pieces[0];
+            if(count($pieces) >= 2){
+                unset($pieces[0]);
+                $domain = implode('.', $pieces);
+            } else {
+                $domain = $_SERVER['SERVER_NAME'];
+            }
+        }
+        return $domain;
+    }
+}
