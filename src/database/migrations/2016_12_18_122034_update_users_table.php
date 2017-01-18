b3e42130f9499aadf445e8dcf594d23eec27355e
Eric Scuccimarra <skooch@gmail.com>
2017-01-18 13:01:29 +0100

    Fixed migration?

--- src/database/migrations/2016_12_18_122034_update_users_table.php
+++ src/database/migrations/2016_12_18_122034_update_users_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use App\User;
 
 class UpdateUsersTable extends Migration
 {
@@ -15,11 +16,23 @@ class UpdateUsersTable extends Migration
      */
     public function up()
     {
-    	Schema::table('users', function (Blueprint $table) {
-	    	$table->integer('type')->unsigned()->default(0);
-    	});
+        if (!Schema::hasColumn('users', 'type')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->integer('type')->unsigned()->default(0);
+            });
+        }
+        if (!Schema::hasColumn('users', 'active')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->boolean('active')->default(1);
+            });
+        }
+        if (!Schema::hasColumn('users', 'image')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->string('image')->nullable();
+            });
+        }
 
-    	$this->seedDatabase();
+        $this->seedDatabase();
     }
 
     /**
@@ -29,8 +42,51 @@ class UpdateUsersTable extends Migration
      */
     public function down()
     {
-    	Schema::table('users', function (Blueprint $table) {
-    		$table->dropColumn('type');
-    	});
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('active');
+            $table->dropColumn('type');
+            $table->dropColumn('image');
+        });
+    }
+
+    private function seedDatabase()
+    {
+        $adminUser = User::where('type', 1)->first();
+
+        if (!count($adminUser)) {
+            DB::table('users')->insert([
+                'name' => 'admin',
+                'email' => 'admin@example.com',
+                'password' => bcrypt('password'),
+                'type' => 1,
+            ]);
+        }
+
+        $tag = DB::table('tags')->where('name', 'test')->first();
+        if (!count($tag)) {
+            DB::table('tags')->insert([
+                'name' => 'test',
+            ]);
+        }
+
+        $blog = DB::table('blogs')->first();
+        if (!count($blog)) {
+            DB::table('blogs')->insert([
+                'user_id' => 1,
+                'title' => 'First Post',
+                'slug' => 'first-post',
+                'body' => 'First post. For test purposes.',
+                'published' => 1,
+                'published_at' => Carbon::now(),
+            ]);
+        }
+
+        $blogtag = DB::table('blog_tag')->first();
+        if(!count($blogtag)){
+            DB::table('blog_tag')->insert([
+                'blog_id' => 1,
+                'tag_id' => 1,
+            ]);
+        }
     }
 }
